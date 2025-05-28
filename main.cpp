61253d2a7d87367e4ed5025e0314ca3cf5ca19d4
Asad Zubair <asad.xnb@gmail.com>
2025-05-28 22:24:49 +0000

    refactored some code and remove unused libs

--- main.cpp
+++ main.cpp
@@ -7,19 +7,22 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <signal.h>
 #include <cstring>
 #include <cstdlib>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <algorithm>
 #include <iostream>
 #include <filesystem>
 
 namespace fs = std::filesystem;
-
 using namespace std;
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
 
 string CURRENT_DIR = ".";
 
